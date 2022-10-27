62f850ed36eaaf286a68597b7aa59b3ae0b3e1e4
zDR9YHt5ooXU <83929232+zDR9YHt5ooXU@users.noreply.github.com>
2022-10-26 21:32:44 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -15,10 +15,96 @@ class Person {
   [SOURCES] = new Source();
 }
 
-console.log('Cloning a source entry');
-const clonedPerson = structuredClone(new Person());
-clonedPerson[SOURCES].greet();
+// console.log('Cloning a source entry');
+// const clonedPerson = structuredClone(new Person());
+// // clonedPerson[SOURCES].greet();
 
-console.log('Cloning a source');
-const clonedSource = structuredClone(new Source());
-clonedSource.greet();
+// console.log('Cloning a source');
+// const clonedSource = structuredClone(new Source());
+// clonedSource.greet();
+
+class OmniSource {
+  constructor(public name: string) {}
+
+  method() {
+    return true;
+  }
+}
+
+const SOURCE = Symbol('source');
+const PRIMARY_KEY = Symbol('primary_key');
+class OmniSourceEntry {
+  [SOURCE]: OmniSource;
+
+  constructor(omniSource: OmniSource, protected semiSecret: string) {
+    this[SOURCE] = omniSource;
+  }
+
+  clone() {
+    return structuredClone(this);
+  }
+}
+
+const SECRET = 'secret';
+const OPEN = 'open';
+const SEMISECRET = 'semiSecret';
+const UPDATED = 'updated';
+
+class InterfaceSourceEntry extends OmniSourceEntry {
+  willBeDiscarded = { [PRIMARY_KEY]: 'primary' };
+  cloneWithoutMethods: { source: OmniSource };
+  willStay = { data: { name: 'name' } };
+  private nestedSecret = { secret: SECRET };
+
+  constructor(
+    private secret: string,
+    public open: string,
+    omniSource: OmniSource,
+    protected semiSecret: string
+  ) {
+    super(omniSource, semiSecret);
+    this.cloneWithoutMethods = {
+      source: omniSource,
+    };
+  }
+
+  setSecret(secret: string) {
+    this.secret = secret;
+  }
+
+  getSecret() {
+    return this.secret;
+  }
+
+  setSemiSecret(secret: string) {
+    this.semiSecret = secret;
+  }
+
+  getSemiSecret() {
+    return this.semiSecret;
+  }
+
+  updateData() {
+    this.willStay.data.name = 'updated';
+  }
+
+  setNestedSecret(secret: string) {
+    this.nestedSecret.secret = secret;
+  }
+
+  getNestedSecret() {
+    return this.nestedSecret.secret;
+  }
+}
+
+const interfaceEntry = new InterfaceSourceEntry(
+  SECRET,
+  OPEN,
+  new OmniSource('interface'),
+  SEMISECRET
+);
+const clonedEntry = interfaceEntry.clone();
+/**
+ * clonedEntry.setSecret should not update private interfaceEntry.secret
+ */
+console.log(clonedEntry.getSecret());
