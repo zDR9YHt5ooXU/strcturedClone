6d149ca2267d10c404bbc08ca11047e779c187fa
zDR9YHt5ooXU <83929232+zDR9YHt5ooXU@users.noreply.github.com>
2022-10-26 21:24:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# strcturedClone
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-fmpk1b)
---
+++ index.ts
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const SOURCES = Symbol('data');
+class Source {
+  greet() {
+    console.log('Hi');
+  }
+}
+class Person {
+  [SOURCES] = new Source();
+}
+
+const clonedPerson = structuredClone(new Person());
+
+clonedPerson.greet();
